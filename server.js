002163c2f0c33ffa34383c0be71d06231dd5f856
Iamtheusername112 <apkfem@gmail.com>
2023-08-28 14:03:05 +0200

    commit

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ app.put("/todo/update/:id", async (req, res) => {
   res.json(todo);
 });
 
-app.listen(3001, () => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
   console.log("Server running on port 3001");
 });
